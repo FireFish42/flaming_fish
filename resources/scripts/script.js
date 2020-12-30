05faa269b06071c6ed94646321eebcd478cddf10
FireFish42 <will.houghton42@gmail.com>
2020-12-30 17:47:11 -0500

    update projects (fire) screen

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -19,10 +19,30 @@ toFishVar.addEventListener("click", toFish);
 
 function toFire() {
     document.getElementById("fire").style.width = "100%";
-    document.getElementById("landing").style.display = "none";
+    document.getElementById("fire-img").style.width = "35%";
+    document.getElementById("landing").style.width = "0%";
+    document.getElementById("return").hidden = false;
 }
 
 function toFish() {
     document.getElementById("fish").style.width = "100%";
-    document.getElementById("landing").style.display = "none";
+    document.getElementById("landing").style.width = "0%";
+}
+
+
+let toLandingVar = document.getElementById("return");
+
+toLandingVar.addEventListener("keyup", function(event) {
+    if (KeyboardEvent.key === "Enter" || KeyboardEvent.key === " ") {
+        event.preventDefault();
+        toFireVar.click();
+    }
+});
+toLandingVar.addEventListener("click", toLanding);
+
+function toLanding() {
+    document.getElementById("fire").style.width = "0%";
+    document.getElementById("fire-img").style.width = "0%";
+    document.getElementById("landing").style.width = "100%";
+    document.getElementById("return").hidden = true;
 }
